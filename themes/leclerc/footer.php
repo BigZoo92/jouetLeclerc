9d0ef4b2e8c29e69e2b47523f5a9cd33c1253171
smoukeuur <enzotang@live.fr>
2022-11-14 14:28:47 +0100

    init landing

--- themes/leclerc/footer.php
+++ themes/leclerc/footer.php
@@ -1,7 +1,10 @@
 <?php  wp_footer(); ?>
+
 <footer>
-        </footer>
-    </div>
+    <img class="pere_noel" src="<?= get_template_directory_uri()?>./assets/images/png/6253503-pere-noel-avec-sapin-noel-vectoriel-removebg-preview.png" alt="">
+    <p>Copyright © 2022 E.Leclerc. Tous droits réservés.</p>
+</footer>
+
 </body>
 
 </html>
