da1a06417755af6597e53e92f78f2ac6083d9fe2
BigZoo92 <givernaudenzo@gmail.com>
2022-11-21 15:54:56 +0100

    init

--- themes/leclerc/functions.php
+++ themes/leclerc/functions.php
@@ -20,19 +20,55 @@ function montheme_register_assets()
     }
 }
 
-function montheme_types()
-{
-    register_post_type('magasins', [
-        'label' => 'Magasins',
+function recettes_register_post_types() {
+	
+    $labels = array(
+        'name' => 'Recettes',
+        'all_items' => 'Toutes les recettes',  // affiché dans le sous menu
+        'singular_name' => 'Recette',
+        'add_new_item' => 'Ajouter un projet',
+        'edit_item' => 'Modifier la recette',
+        'menu_name' => 'Recettes'
+    );
+
+	$args = array(
+        'labels' => $labels,
         'public' => true,
-        'supports' => ['title', 'thumbnail'],
         'show_in_rest' => true,
         'has_archive' => true,
-    ]);
+        'supports' => array( 'title', 'editor','thumbnail' ),
+        'menu_position' => 5, 
+        'menu_icon' => get_template_directory_uri() . '/assets/images/png/recipe_icon.png',
+	);
+
+	register_post_type( 'recettes', $args );
 }
 
+function cadeaux_register_post_types() {
+	
+    $labels = array(
+        'name' => 'Cadeaux',
+        'all_items' => 'Tous les cadeaux',  // affiché dans le sous menu
+        'singular_name' => 'Cadeau',
+        'add_new_item' => 'Ajouter un cadeau',
+        'edit_item' => 'Modifier le cadeau',
+        'menu_name' => 'Cadeaux'
+    );
 
-add_action('init', 'montheme_types');
+	$args = array(
+        'labels' => $labels,
+        'public' => true,
+        'show_in_rest' => true,
+        'has_archive' => true,
+        'supports' => array( 'title', 'editor','thumbnail' ),
+        'menu_position' => 5, 
+        'menu_icon' => get_template_directory_uri() . '/assets/images/png/gift_icon.png',
+	);
+
+	register_post_type( 'cadeaux', $args );
+}
+add_action( 'init', 'recettes_register_post_types' ); 
+add_action( 'init', 'cadeaux_register_post_types' ); 
 
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
 
