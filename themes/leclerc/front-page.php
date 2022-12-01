9d9582a4cab2ee99b729e878e5046faf73e721f4
BigZoo92 <givernaudenzo@gmail.com>
2022-12-01 12:03:13 +0100

    init cometes

--- themes/leclerc/front-page.php
+++ themes/leclerc/front-page.php
@@ -1,12 +1,19 @@
 <?php get_header(); ?>
 
 <main>
+    <div class="cd_cometes">
+        <div class="comets">
+            <i></i>
+            <i></i>
+            <i></i>
+        </div>
+    </div>
 
     <section class="hero">
-        <img id="boite_lettre" src="<?= get_template_directory_uri()?>./assets/images/svg/boite_aux_lettres.svg" title="Envoyer une lettre au père noël" alt="Boite aux lettres">
-        <img id="sapin" src="<?= get_template_directory_uri()?>./assets/images/svg/sapin.svg" title="Afficher" alt="Sapin">
-        <img id="lutin" src="<?= get_template_directory_uri()?>./assets/images/png/lutin.png" title="Accéder au lutin" alt="Lutin">
-        <img id="lutin2" src="<?= get_template_directory_uri()?>./assets/images/png/lutin.png" title="Afficher" alt="Lutin n2">
+        <img id="boite_lettre" src="<?= get_template_directory_uri() ?>./assets/images/svg/boite_aux_lettres.svg" title="Envoyer une lettre au père noël" alt="Boite aux lettres">
+        <img id="sapin" src="<?= get_template_directory_uri() ?>./assets/images/svg/sapin.svg" title="Afficher" alt="Sapin">
+        <img id="lutin" src="<?= get_template_directory_uri() ?>./assets/images/png/lutin.png" title="Accéder au lutin" alt="Lutin">
+        <img id="lutin2" src="<?= get_template_directory_uri() ?>./assets/images/png/lutin.png" title="Afficher" alt="Lutin n2">
     </section>
 </main>
 
