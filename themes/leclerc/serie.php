074da60ef511e376ccef5d2b6011d0d9a6f849f8
smoukeuur <enzotang@live.fr>
2022-12-09 16:20:23 +0100

    init seri

---
+++ themes/leclerc/serie.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Série
+ */
+get_header()
+?>
+
+<main id="bg_serie">
+    <section class="serie">
+        <div class="header_serie">
+            <h1>Série</h1>
+        </div>
+
+        <div class="corps_serie">
+            <div class="box_serie">
+                <h2>Titre de la série</h2>
+                <img id="houx_gauche" src="<?= get_template_directory_uri()?>./assets/images/png/377-3773713_christmas-corner-decorations-png-download-christmas-frame-corner.png" alt="">
+                <video controls src="<?= get_template_directory_uri()?>./assets/videos/Lighted Christmas Tree.mp4"></video>
+                <img id="houx_droite" src="<?= get_template_directory_uri()?>./assets/images/png/377-3773713_christmas-corner-decorations-png-download-christmas-frame-corner.png" alt="">
+            </div>
+
+            <div class="box_serie">
+                <h2>Titre de la série</h2>
+                <img id="houx_gauche" src="<?= get_template_directory_uri()?>./assets/images/png/377-3773713_christmas-corner-decorations-png-download-christmas-frame-corner.png" alt="">
+                <video controls src="<?= get_template_directory_uri()?>./assets/videos/Lighted Christmas Tree.mp4"></video>
+                <img id="houx_droite" src="<?= get_template_directory_uri()?>./assets/images/png/377-3773713_christmas-corner-decorations-png-download-christmas-frame-corner.png" alt="">
+            </div>
+
+            <div class="box_serie">
+                <h2>Titre de la série</h2>
+                <img id="houx_gauche" src="<?= get_template_directory_uri()?>./assets/images/png/377-3773713_christmas-corner-decorations-png-download-christmas-frame-corner.png" alt="">
+                <video controls src="<?= get_template_directory_uri()?>./assets/videos/Lighted Christmas Tree.mp4"></video>
+                <img id="houx_droite" src="<?= get_template_directory_uri()?>./assets/images/png/377-3773713_christmas-corner-decorations-png-download-christmas-frame-corner.png" alt="">
+            </div>
+        </div>
+    </section>
+
+</main>
+
+
+<?php get_footer() ?>
