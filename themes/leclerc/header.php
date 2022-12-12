7c6453e286ece32daa6e48877f97e940d079b81c
smoukeuur <enzotang@live.fr>
2022-12-12 23:42:48 +0100

    ajout background header

--- themes/leclerc/header.php
+++ themes/leclerc/header.php
@@ -5,18 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?= get_template_directory_uri()?>./assets/images/ico/favicon.ico"
+    <link rel="shortcut icon" href="<?= get_template_directory_uri() ?>./assets/images/ico/favicon.ico"
         type="image/x-icon">
     <?php wp_head(); ?>
 </head>
 
 <body>
     <!-- <?php //wp_nav_menu([
-                    //'menu' => 'Menu 1',
-                    //'theme_location' => 'header',
-                    //'container' => "nav",
-                //]);
-                ?> -->
+    //'menu' => 'Menu 1',
+    //'theme_location' => 'header',
+    //'container' => "nav",
+    //]);
+    ?> -->
     <header>
         <div class="overlay_header">
             <nav>
@@ -42,7 +42,7 @@
             </div>
         </div>
         <div class="sapin_toggle">
-            <img src="<?= get_template_directory_uri()?>./assets/images/svg/star.svg" alt="Etoile sapin">
+            <img src="<?= get_template_directory_uri() ?>./assets/images/svg/star.svg" alt="Etoile sapin">
             <div class="branches">
                 <div class="branche arm1"></div>
                 <div class="branche arm2"></div>
@@ -56,7 +56,7 @@
         </div>
 
         <!-- <div class="logo_leclerc">
-            <img src="<?= get_template_directory_uri()?>./assets/images/png/leclerc.png" alt="Logo Leclerc">
+            <img src="<?= get_template_directory_uri() ?>./assets/images/png/leclerc.png" alt="Logo Leclerc">
         </div> -->
 
         <div class="modal_container">
@@ -93,7 +93,13 @@
 
         </div>
     </header>
-
+    <div class="custom-shape-divider-top-1670883178">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                class="shape-fill"></path>
+        </svg>
+    </div>
     <div class="overlay_portrait">
         <h1>Tourne ton écran</h1>
         <i class="bi bi-phone-vibrate"></i>
