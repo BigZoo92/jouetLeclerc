34775fe6eb19240031f4474d9ec62bcc0cd2a7e9
smoukeuur <enzotang@live.fr>
2023-01-08 22:54:54 +0100

    Desactiver Admin Bar

--- themes/leclerc/functions.php
+++ themes/leclerc/functions.php
@@ -39,6 +39,13 @@ function my_login_redirect( $redirect_to, $request, $user ) {
 	}
 }
 
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
 
 
 
