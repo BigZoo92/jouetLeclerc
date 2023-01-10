fbdaf3de4dd3519656d71b35a9b48ddd2e598885
BigZoo92 <givernaudenzo@gmail.com>
2023-01-10 18:19:16 +0100

    finish calendrier

--- themes/leclerc/assets/js/calendrier.js
+++ themes/leclerc/assets/js/calendrier.js
@@ -3,15 +3,32 @@ const cadeau = document.querySelectorAll('.cadeau')
 cadeau.forEach((e) => {
 e.addEventListener('click', () => {
     e.style.position = "absolute"
-    console.log(e.style.position);
     e.style.left = "50%";
     e.style.top = "50%";
-    e.style.transform = "transform(-50%, -50%)"
+    e.style.transform = "translate(-50%, -50%)"
     e.style.setProperty("--tailleCadeau", "30vw")
     document.querySelectorAll('.cadeau').forEach((item) => {
         item.style.opacity = "0"
         item.pointerEvents = "none"
     })
     e.style.opacity = "1"
+    const couvercle = e.childNodes[1];
+    setTimeout(() => {
+        couvercle.style.top = "-27.5vh";
+        
+        setTimeout(() => {
+            couvercle.style.transform = "rotate(-90deg)";
+            couvercle.style.left = "-10vh";
+            setTimeout(() => {
+                couvercle.style.opacity = "0";
+                e.style.opacity = "0";
+                setTimeout(() => {
+                    couvercle.style.display = "none";
+                    e.style.display = "none";
+                }, 1000);
+            }, 100);
+        }, 100);
+    }, 1500);
+    
     })
 })
--- themes/leclerc/calendrier.php
+++ themes/leclerc/calendrier.php
@@ -9,81 +9,107 @@ get_header()
 <main class="tonperetang">
 
     <section class="wrap_sapin_calendrier">
+        <h1>Calendrier</h1>
         <div class="sapin_calendrier">
             <div class="cadeau c1">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c2">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c3">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c4">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c5">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c6">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c7">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c7">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c8">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c9">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c10">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c11">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c12">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c13">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c14">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c15">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c16">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c17">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c18">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c19">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c20">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c21">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c22">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c23">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
             <div class="cadeau c24">
                 <div class="couvercle"></div>
+                <div class="num"></div>
             </div>
         </div>
 
